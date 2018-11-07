fd9e15b7076bdacbf92c4562ae53301ee87f629d
zxl <14787484930@163.com>
2018-11-07 22:48:40 +0800

    创建控制器 创建控制层测试类

--- src/main/java/com/ebook/controllers/book/BookController.java
+++ src/main/java/com/ebook/controllers/book/BookController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -17,11 +18,11 @@ public class BookController {
     @Autowired
     BookService bookService;
 
-    @RequestMapping("books")
-    public Object getBooks(Model model){
+    @RequestMapping("/books")
+    public Object getBooks(Model model,@RequestParam(value = "pn", defaultValue = "2") Integer pn){
 
         //在查询之前注入页码和页面容量
-        PageHelper.startPage(1,2);
+        PageHelper.startPage(pn,2);
         //查询数据
         List<Book> list = bookService.getBooks();
         //包装查询后的结果
---
+++ src/test/java/TestController.java
@@ -0,0 +1,70 @@
+import com.ebook.beans.book.Book;
+import com.github.pagehelper.PageInfo;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.List;
+
+/**
+ * 测试Controller
+ * zxl
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(locations = {"classpath:applicationContext.xml","file:src/main/webapp/WEB-INF/dispatcherServlet-servlet.xml"})
+public class TestController {
+
+    @Autowired
+    WebApplicationContext context;
+
+    //虚拟MVC请求，获取请求结果
+    MockMvc mockMvc;
+
+    @Before
+    public void init(){
+        mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+    }
+
+    @Test
+    public void test() throws Exception {
+
+        //发送get请求
+        MvcResult result =  mockMvc.perform(MockMvcRequestBuilders.get("/books").param("pn","2")).andReturn();
+
+        MockHttpServletRequest request = result.getRequest();
+
+        PageInfo<Book> pageInfo = (PageInfo<Book>) request.getAttribute("pageinfo");
+
+        System.out.println(pageInfo);
+        System.out.println("当前页码:"+pageInfo.getPageNum());
+        System.out.println("页数"+pageInfo.getPages());
+        System.out.println("总数："+pageInfo.getTotal());
+
+        System.out.println("要显示的页码：");
+
+        int[] nums = pageInfo.getNavigatepageNums();
+
+        for (int i=0;i<nums.length;i++){
+
+            System.out.print(nums[i]+" ");
+        }
+
+        System.out.println("输出图书数据：");
+        List<Book> list = pageInfo.getList();
+        for(Book book:list){
+            System.out.println("书名："+book.getName()+" 图片："+book.getPic()+" 价格："+book.getPrice());
+        }
+
+    }
+}
