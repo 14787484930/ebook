fad91c198d508156c8715c37d1e31c6db2b18e70
zxl <14787484930@163.com>
2018-11-11 14:44:11 +0800

    引入redis缓存

---
+++ src/main/java/com/model/utills/redis/RedisCache.java
@@ -0,0 +1,169 @@
+package com.model.utills.redis;
+
+import org.apache.ibatis.cache.Cache;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnection;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import redis.clients.jedis.exceptions.JedisConnectionException;
+
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class RedisCache implements Cache {
+
+    private static final Logger logger = LoggerFactory.getLogger(RedisCache.class);
+
+    private static JedisConnectionFactory jedisConnectionFactory;
+
+    private final String id;
+
+    /**
+     * The {@code ReadWriteLock}.
+     */
+    private final ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+
+    public RedisCache(final String id) {
+        if (id == null) {
+            throw new IllegalArgumentException("Cache instances require an ID");
+        }
+        logger.debug("MybatisRedisCache:id=" + id);
+        this.id = id;
+    }
+
+    @Override
+    public void clear()
+    {
+        JedisConnection connection = null;
+        try
+        {
+            connection = jedisConnectionFactory.getConnection(); //连接清除数据
+            connection.flushDb();
+            connection.flushAll();
+        }
+        catch (JedisConnectionException e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            if (connection != null) {
+                connection.close();
+            }
+        }
+    }
+
+    @Override
+    public String getId()
+    {
+        return this.id;
+    }
+
+    @Override
+    public Object getObject(Object key)
+    {
+        Object result = null;
+        JedisConnection connection = null;
+        try
+        {
+            connection = jedisConnectionFactory.getConnection();
+            RedisSerializer<Object> serializer = new JdkSerializationRedisSerializer(); //借用spring_data_redis.jar中的JdkSerializationRedisSerializer.class
+            result = serializer.deserialize(connection.get(serializer.serialize(key))); //利用其反序列化方法获取值
+        }
+        catch (JedisConnectionException e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            if (connection != null) {
+                connection.close();
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public ReadWriteLock getReadWriteLock()
+    {
+        return this.readWriteLock;
+    }
+
+    @Override
+    public int getSize()
+    {
+        int result = 0;
+        JedisConnection connection = null;
+        try
+        {
+            connection = jedisConnectionFactory.getConnection();
+            result = Integer.valueOf(connection.dbSize().toString());
+        }
+        catch (JedisConnectionException e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            if (connection != null) {
+                connection.close();
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public void putObject(Object key, Object value)
+    {
+        JedisConnection connection = null;
+        try
+        {
+            logger.info(">>>>>>>>>>>>>>>>>>>>>>>>putObject:"+key+"="+value);
+            connection = jedisConnectionFactory.getConnection();
+            RedisSerializer<Object> serializer = new JdkSerializationRedisSerializer(); //借用spring_data_redis.jar中的JdkSerializationRedisSerializer.class
+            connection.set(serializer.serialize(key), serializer.serialize(value)); //利用其序列化方法将数据写入redis服务的缓存中
+
+        }
+        catch (JedisConnectionException e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            if (connection != null) {
+                connection.close();
+            }
+        }
+    }
+
+    @Override
+    public Object removeObject(Object key)
+    {
+        JedisConnection connection = null;
+        Object result = null;
+        try
+        {
+            connection = jedisConnectionFactory.getConnection();
+            RedisSerializer<Object> serializer = new JdkSerializationRedisSerializer();
+            result =connection.expire(serializer.serialize(key), 0);
+        }
+        catch (JedisConnectionException e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            if (connection != null) {
+                connection.close();
+            }
+        }
+        return result;
+    }
+
+    public static void setJedisConnectionFactory(JedisConnectionFactory jedisConnectionFactory) {
+        RedisCache.jedisConnectionFactory = jedisConnectionFactory;
+    }
+
+}
