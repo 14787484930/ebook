b25818c0a43d18040a2779941cb8b3017bf8e9c5
zxl <14787484930@163.com>
2018-11-13 00:42:44 +0800

    编写响应数据的封装工具类，并且测通

--- src/main/java/com/ebook/controllers/book/BookController.java
+++ src/main/java/com/ebook/controllers/book/BookController.java
@@ -4,6 +4,7 @@ import com.ebook.beans.book.Book;
 import com.ebook.services.BookService;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import com.model.utills.messages.ResultInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -30,8 +31,23 @@ public class BookController {
         List<Book> list = bookService.getBooks();
         //包装查询后的结果
         PageInfo<Book> pageInfo = new PageInfo<Book>(list,3);
-        return pageInfo;
+        //封装数据
+        return ResultInfo.success().add("pageinfo",pageInfo);
     }
+
+
+    /*@RequestMapping("/books")
+    @ResponseBody
+    public Object getBooks(@RequestParam(value = "pn", defaultValue = "2") Integer pn , Model model){
+
+        //在查询之前注入页码和页面容量
+        PageHelper.startPage(pn,2);
+        //查询数据
+        List<Book> list = bookService.getBooks();
+        //包装查询后的结果
+        PageInfo<Book> pageInfo = new PageInfo<Book>(list,3);
+        return pageInfo;
+    }*/
 
    /* @RequestMapping("/books")
     public Object getBooks(@RequestParam(value = "pn", defaultValue = "2") Integer pn ,Model model){
--- src/main/java/com/model/utills/messages/ResultInfo.java
+++ src/main/java/com/model/utills/messages/ResultInfo.java
@@ -1,4 +1,44 @@
 package com.model.utills.messages;
 
+import lombok.Data;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * zxl
+ * 2018-11-12
+ * 客户端请求返回结果封装类
+ */
+@Data
 public class ResultInfo {
+
+    //返回状态码{100：SUCCESS，200：fail}
+    private Integer code;
+
+    //异常等提示信息
+    private Map<String,Object> msgs = new HashMap<String,Object>();
+
+    //pageInfo数据（回馈用户的主要数据）
+    private  Map<String,Object> pageinfo = new HashMap<String,Object>();
+
+
+    public static ResultInfo success(){
+        ResultInfo result = new ResultInfo();
+        result.setCode(100);
+        result.msgs.put("msg","处理成功！");
+        return result;
+    }
+
+    public static ResultInfo fail(){
+        ResultInfo result = new ResultInfo();
+        result.setCode(200);
+        result.msgs.put("msg","处理失败！");
+        return result;
+    }
+
+    public ResultInfo add(String key,Object value){
+        this.pageinfo.put(key, value);
+        return this;
+    }
 }
