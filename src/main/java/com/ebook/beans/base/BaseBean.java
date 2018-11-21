54744e65dd082ff013a520d07b9112df67fe6f00
zxl <14787484930@163.com>
2018-11-21 23:52:53 +0800

    创建实体基类

---
+++ src/main/java/com/ebook/beans/base/BaseBean.java
@@ -0,0 +1,14 @@
+package com.ebook.beans.base;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class BaseBean {
+
+    public String createUser; //创建者，后续会用微信来做
+    public Date createTime;  //创建时间
+    public String updateUser; //修改人
+    public Date updateTime; //修改时间
+}
