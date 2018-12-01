3973a54ddcdb3547bbbf330a2699a60634285907
zxl <14787484930@163.com>
2018-12-02 01:56:23 +0800

    图片上传和删除工具类的编写

---
+++ src/main/java/com/model/utills/upload/PicUpload.java
@@ -0,0 +1,57 @@
+package com.model.utills.upload;
+
+import com.model.utills.uuid.GeneratingId;
+import net.coobird.thumbnailator.Thumbnails;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpSession;
+import java.io.File;
+
+/**
+ * 图片上传
+ */
+public class PicUpload {
+
+    /*x新增时上传图片*/
+    public static String uploadPic(MultipartFile[] pics, HttpSession session,String type)throws Exception{
+
+        StringBuffer sb = new StringBuffer();
+
+        //获取文件上传路径
+        String path = session.getServletContext().getRealPath("/pictures/" + type);
+
+        for(MultipartFile pic : pics) {
+
+            String endStr = StringUtils.substringAfterLast(pic.getOriginalFilename(),".");
+
+            File file = new File(path, GeneratingId.getId() + "." + endStr);
+
+            Thumbnails.of(pic.getInputStream()).scale(1f).outputQuality(0.25f).toFile(file);
+
+            sb.append("/pictures/" + type + "/").append(file.getName()).append(",");
+
+        };
+        return sb.deleteCharAt(sb.length()-1).toString();
+    }
+
+    /*编辑时删除原来图片*/
+    public static boolean delPic(String picsPath,HttpSession session){
+
+        //获取文件上传路径
+        String path = session.getServletContext().getRealPath("/");
+
+        //解析出单个图片的路径
+        String[] pics = StringUtils.split(picsPath,",");
+
+        for(String pic:pics){
+
+            path = StringUtils.replace(path,"\\","/");
+            pic = StringUtils.substringAfter(pic,"/");
+            File file = new File(path+pic);
+            file.delete();
+        }
+
+        return true;
+    }
+}
