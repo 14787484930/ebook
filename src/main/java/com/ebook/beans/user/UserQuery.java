290ff30a2dd4ca9a78ceac3edf3129af5833d5a5
zxl <14787484930@163,com>
2018-12-20 17:56:31 +0800

    用户查询类

---
+++ src/main/java/com/ebook/beans/user/UserQuery.java
@@ -0,0 +1,14 @@
+package com.ebook.beans.user;
+
+import lombok.Data;
+
+/**
+ * @author zxl
+ * @date 2018/12/20 17:51
+ * @describe
+ */
+@Data
+public class UserQuery {
+
+    private String id;
+}
