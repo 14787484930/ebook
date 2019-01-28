a495d318d7b4a3b9da1a90da51f9c62a0484e484
zxl <14787484930@163,com>
2019-01-28 16:32:56 +0800

    举报查询参数的添加

--- src/main/java/com/ebook/beans/reportuser/ReportProductQuery.java
+++ src/main/java/com/ebook/beans/reportuser/ReportProductQuery.java
@@ -6,7 +6,7 @@ import lombok.Data;
 @Data
 public class ReportProductQuery extends BaseQuery {
 
-    private String name; //产品名称
+    private String productName; //产品名称
     private String reportType; //举报类型
     private Integer productType; //产品类型 {1：图书，2：电子，3：其他}
     private String productId; //产品编号
