6d688eafb6f1855d035134fe113740457218ff2b
zxl <14787484930@163.com>
2019-04-06 18:51:42 +0800

    辅导校验修改

--- src/main/java/com/ebook/beans/tutoring/Tutoring.java
+++ src/main/java/com/ebook/beans/tutoring/Tutoring.java
@@ -25,15 +25,15 @@ public class Tutoring extends BaseBean  {
     private Double price; //辅导价格
 
     @Future(message = "日期不合法")
-    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm")
     private Date startTime; //开始时间
 
     @Future(message = "日期不合法")
-    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm")
     private Date endTime; //结束时间
 
-    /*@Future(message = "日期不合法")
-    @DateTimeFormat(pattern="yyyy-MM-dd")*/
+    @Future(message = "日期不合法")
+    @DateTimeFormat(pattern="yyyy-MM-dd")
     private Date endDate; //辅导截止日期
 
     private String place; //讲座地点
