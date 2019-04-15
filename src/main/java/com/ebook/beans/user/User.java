d3e75b4fc136b11f5da5a72b14e610674acfb220
zxl <14787484930@163,com>
2019-04-15 17:36:10 +0800

    加入字段

--- src/main/java/com/ebook/beans/user/User.java
+++ src/main/java/com/ebook/beans/user/User.java
@@ -20,6 +20,8 @@ public class User implements Serializable {
     private Double score; //用户评分
     private Integer scoreNumber; //评分次数
 
+    private Integer flag; //{0：我要购买，1：我要发布}
+
     /*认证所需字段*/
     @NotEmpty(message = "学号不能为空！")
     private String studNo;     //学号
