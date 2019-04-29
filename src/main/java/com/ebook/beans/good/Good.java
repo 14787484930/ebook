83f710bc7e4afe72ad0170aef0b6e619e3e61079
zxl <14787484930@163,com>
2019-04-29 17:38:10 +0800

    商品类

---
+++ src/main/java/com/ebook/beans/good/Good.java
@@ -0,0 +1,16 @@
+package com.ebook.beans.good;
+
+import com.ebook.beans.base.BaseBean;
+
+/**
+ * @author zxl
+ * @date 2019/4/29 9:36
+ * @describe
+ */
+public class Good extends BaseBean {
+
+    private String goodName;
+    private String des;
+
+
+}
