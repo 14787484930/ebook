c955936c87ae701bb087e268c684878e41acc989
zxl <14787484930@163.com>
2020-01-04 23:39:49 +0800

    将微信token类注入到spring容器

---
+++ src/main/java/com/ebook/container/WeiXinToken.java
@@ -0,0 +1,13 @@
+package com.ebook.container;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class WeiXinToken {
+
+    //调用接口凭据
+    @Autowired
+    public static String ACCESSTOKEN = "17_uJBHxd55DocGY8bE-MX0RRG1IB4zqs-pcW8jvh1RjB5QmWHYHwo3pkNSX-f7A4kmYSM78fnZiFx9ERjwddZXkONftugvhmfdEE_lLcNpXgS6aafCn8-IEM4iE5WlwHU5I1cDA5lviuP7JnlQLGYeAAADYQ";
+
+}
