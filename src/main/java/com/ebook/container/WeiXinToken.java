83cd6cff85b1dc2f9e6a57d24adc4e7447dea700
zxl <14787484930@163.com>
2020-01-08 23:14:26 +0800

    修改盘符

--- src/main/java/com/ebook/container/WeiXinToken.java
+++ src/main/java/com/ebook/container/WeiXinToken.java
@@ -8,6 +8,6 @@ public class WeiXinToken {
 
     //调用接口凭据
     @Autowired
-    public static String ACCESSTOKEN = "17_uJBHxd55DocGY8bE-MX0RRG1IB4zqs-pcW8jvh1RjB5QmWHYHwo3pkNSX-f7A4kmYSM78fnZiFx9ERjwddZXkONftugvhmfdEE_lLcNpXgS6aafCn8-IEM4iE5WlwHU5I1cDA5lviuP7JnlQLGYeAAADYQ";
+    public static String ACCESSTOKEN = "";
 
 }
